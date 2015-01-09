dcb32c15507fe9a2102c6405516a27d922cc0753
Misho <djzoning@gmail.com>
2015-01-09 20:01:33 +0200

    Add different views for different users

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -1,5 +1,14 @@
 'use strict';
 
-app.controller('LoginController', function($scope, $location, authService, notifyService){
-    //TODO
+app.controller('LoginController', function($scope, $rootScope, $location, authService, notifyService){
+    $scope.login = function(userData){
+        authService.login(userData,
+        function success(data){
+            notifyService.showInfo('Login successful');
+            $location.path('/');
+        },
+        function error(error){
+            notifyService.showError('Login unsuccessful', error);
+        })
+    }
 });
