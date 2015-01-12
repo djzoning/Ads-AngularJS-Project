f4e671956b0b103131164df59bc6a238a3a7687e
Misho <djzoning@gmail.com>
2015-01-12 22:01:36 +0200

    Hide right sidebar when it needs

--- app/js/controllers/RightSidebarController.js
+++ app/js/controllers/RightSidebarController.js
@@ -1,16 +1,23 @@
 'use strict';
 
-app.controller('RightSidebarController', function($scope, $rootScope, categoriesService, townsService){
+app.controller('RightSidebarController', function($scope, $rootScope, $location,
+        categoriesService, townsService){
     $scope.categories = categoriesService.getCategories();
     $scope.towns = townsService.getTowns();
 
     $scope.categoryClicked = function(clickedCategoryId){
         $scope.selectedCategoryId = clickedCategoryId;
         $rootScope.$broadcast("categorySelectionChanged", clickedCategoryId);
-    }
+    };
 
     $scope.townClicked = function(clickedTownId){
         $scope.selectedTownId = clickedTownId;
         $rootScope.$broadcast("townSelectionChanged", clickedTownId);
+    };
+
+    $scope.checkUrl = function(){
+        return $location.path().indexOf('/user') == -1
+            && $location.path().indexOf('/login') == -1
+            && $location.path().indexOf('/register') == -1;
     }
 });
